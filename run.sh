845766487eeebdf56d147beb168835226eb708dc
Yash Gajera <yash.gajera@outlook.com>
2022-04-30 16:16:35 -0500

    backup of apartment

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 source /home/gajerya/source/apartment_check_sms/venv/bin/activate
 python3 source/apartment_check_sms/apartment_check.py $1
+cp source/apartment_check_sms/apartment_list.json source/apartment_check_sms/apartment_list.json.bak
